32ca62d63aa44e6a1a1609b015d4f79e66ad49b8
br-lovanshi <brajeshlovanshi2001@gmail.com>
2025-12-04 20:34:02 +0530

    single responsibility principle

--- src/SolidPrinciple/SingleResponsibility.java
+++ src/SolidPrinciple/SingleResponsibility.java
@@ -1,4 +1,30 @@
 package SolidPrinciple;
+// A class should be responsible for single operation.
+
+// problem
+class Invoice{
+    public void calculation(){}
+    void generateInvoice(){}
+    void print(){}
+}
+
+// solution
+class Calculation{
+    void calculateBill(){}
+}
+
+ class InvoiceGenerator{
+    void generatorInvoice(){}
+}
+
+ class Print{
+    void print(){}
+}
 
 public class SingleResponsibility {
+    public static void main(String args[]){
+        Calculation cal = new Calculation();
+        InvoiceGenerator generator = new InvoiceGenerator();
+        Print print = new Print();
+}
 }

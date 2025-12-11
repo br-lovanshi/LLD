2b9d9774454f01d29f77af619eb12e8ac75721d7
br-lovanshi <brajeshlovanshi2001@gmail.com>
2025-12-11 17:23:43 +0530

    facade

--- src/DesingPatterns/Structural/Facade.java
+++ src/DesingPatterns/Structural/Facade.java
@@ -1,4 +1,23 @@
-package DesingPatterns.Creational.Structural;
+package DesingPatterns.Structural;
 
+class ValetParking{
+
+}
+class FoodService{
+
+}
+class HouseKeeping{
+
+}
+class HotelFacade{
+
+    ValetParking valetParking;
+    FoodService foodService;
+    HouseKeeping houseKeeping;
+}
 public class Facade {
+    public static void main(String[] args) {
+        HotelFacade hotelFacade = new HotelFacade();
+
+    }
 }
